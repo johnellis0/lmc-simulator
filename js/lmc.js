7bc46bb61b0be5d83279de8ea5e149ae65de052b
John Ellis <34400721+johnellis0@users.noreply.github.com>
2020-11-24 18:52:47 +0000

    Memory cell getters/setters

--- js/lmc.js
+++ js/lmc.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function getMemoryCell(address){
+    var cell = document.getElementById("mem_input_" + address);
+    return cell.value;
+}
+
+function setMemoryCell(address, value){
+    var cell = document.getElementById("mem_input_" + address);
+    cell.value = value;
+}
+
 export default function LMC(){
     this.pc = 0;
     this.acc = 0;
@@ -7,5 +17,7 @@ export default function LMC(){
 
 LMC.prototype = {
     ...LMC.prototype,
+    cycle: function() {
 
+    }
 }
