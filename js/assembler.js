3741f4c8c69a659766bc405f593ff20e561dbdc2
John Ellis <34400721+johnellis0@users.noreply.github.com>
2020-11-24 21:37:50 +0000

    Add basic instruction assembling and comment handling

--- js/assembler.js
+++ js/assembler.js
@@ -1,47 +1,58 @@
 "use strict";
 
-const instructions = {
-    ADD: 1,
-    SUB: 2,
-    STA: 3,
-    LDA: 5,
-    BRA: 6,
-    BRZ: 7,
-    BRP: 8,
-    INP: 901,
-    OUT: 902,
-    HLT: 0
-};
+const commentPrefix = "//";
+const opcodes = {ADD: "1", SUB: "2", STA: "3", LDA: "5", BRA: "6", BRZ: "7", BRP: "8", INP: "901", OUT: "902", HLT: "000"};
+const requiresAddress = [opcodes.ADD, opcodes.SUB, opcodes.STA, opcodes.LDA, opcodes.BRA, opcodes.BRZ, opcodes.BRP];
+
+/**
+ * Returns opcode & address from text or null if instruction not present
+ * @param text
+ * @returns {{address: null, opcode: *}|null}
+ */
+function parseInstruction(text){
+    if(text.indexOf(commentPrefix) !== -1)
+        text = text.substring(0, text.indexOf(commentPrefix));
+
+    var opcode = null, address = null;
+
+    var splits = text.split(" ").filter(t => t !== "");
+
+    if(splits.length === 0)
+        return null;
+    if(splits.length > 2)
+        throw "Invalid instruction: "+ text;
+
+    if(Object.keys(opcodes).includes(splits[0])) {
+        opcode = opcodes[splits[0]];
+
+        if (requiresAddress.includes(opcode)) { // if address required
+            if (splits.length !== 2)
+                throw "Invalid instruction: " + text;
+
+            address = splits[1].padStart(2, "0");
+        }else{
+            if (splits.length !== 1)
+                throw "Invalid instruction: " + text;
+        }
+    }else{
+        throw "Invalid instruction: " + text;
+    }
+
+    return {opcode, address}
+}
 
 export default function assemble(program){
     var lines = program.split("\n");
-    console.log(lines);
-    var machineCode = [];
+
+    var instructions = [];
+    var labels = [];
 
     lines.forEach((line) => {
-        console.log(line);
-        var words = line.split(" ");
-        console.log(words);
-        var instruction = words[0];
+        var instruction = parseInstruction(line);
 
-        if(!Object.keys(instructions).includes(instruction)){
-            throw "Invalid instruction";
-        }
+        if(instruction !== null)
+            instructions.push(instruction.opcode + (instruction.address === null ? "" : instruction.address));
+    });
 
-        if(["INP","OUT","HLT"].includes(instruction)){
-            if(words.length > 1){
-                throw "Invalid instruction";
-            }
-            machineCode.push(instructions[instruction]);
-        }else{
-            if(words.length !== 2){
-                throw "Invalid instruction";
-            }
-            var address = words[1];
-
-            machineCode.push(instructions[instruction] + address);
-        }
-    })
-
-    return machineCode.join("\n");
+    return instructions;
 }
